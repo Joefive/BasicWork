37b51ccaaeba171a3effa926a8bd42ef2108cd8f
Joefive <joefive@yeah.net>
2020-04-29 11:39:45 +0800

    ignored

--- Basicode/src/com/sunrise/day05/Demo03Static.java
+++ Basicode/src/com/sunrise/day05/Demo03Static.java
@@ -1,5 +1,6 @@
 package com.sunrise.day05;
 
+
 /**
  * static来修饰成员变量，该成员变量将不再属于该对象，而是数据整个类
  * static来修改成员方法，该成员方法将不在属于该对象，属于整个类
@@ -14,10 +15,13 @@ public class Demo03Static {
         System.out.println("学号："+st1.getId()+"姓名："+st1.getName()+"年龄："+st1.getAge()+"教室："+st1.room);
         Student st2 = new Student("张二",20);
         System.out.println("学号："+st2.getId()+"姓名："+st2.getName()+"年龄："+st2.getAge()+"教室："+st2.room);
+        System.out.println("=================");
 
-        StaticMethod st = new StaticMethod();
-        st.method();
-        st.methodStatic(); //不推荐写法
         StaticMethod.methodStatic(); //推荐写法：类名+静态方法名
+//        StaticMethod.method();    //非静态方法不new无法使用
+//        StaticMethod st = new StaticMethod();
+//        st.method();
+//        st.methodStatic(); //不推荐写法
+
     }
 }
