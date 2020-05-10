927de587866ee26fc1c9d8a2bb2f957ce53a0bcc
Joefive <joefive@yeah.net>
2020-05-11 00:05:38 +0800

    1

---
+++ Basicode/src/com/sunrise/day11/Demo01Collection.java
@@ -0,0 +1,14 @@
+package com.sunrise.day11;
+
+/**
+ * 学习集合目标：
+ * 1.使用集合存储数据；
+ * 2.会遍历集合里面的元素；
+ * 3.掌握每种集合的特性;
+ */
+
+public class Demo01Collection {
+    public static void main(String[] args) {
+
+    }
+}
