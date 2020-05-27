bb33b83f25c0e7621c65cf940cac775057a9a7a0
Joefive <joefive@yeah.net>
2020-05-27 22:00:29 +0800

    1

--- Basicode/src/com/sunrise/day15/Lambda/Demo02Lambda.java
+++ Basicode/src/com/sunrise/day15/Lambda/Demo02Lambda.java
@@ -1,13 +1,16 @@
 package com.sunrise.day15.Lambda;
 
 /**
- * Lambda表达式：函数式编程，不是面向对象思想
+ * Lambda表达式：函数式编程，不是面向对象思想，只要针对匿名对象，匿名接口使用
  * 一个参数，一个箭头，一段代码
  * 1. 不需要参数,返回值为5： () -> 5
  * 2. 接收一个参数(数字类型),返回其2倍的值：x -> 2 * x
  * 3. 接受2个参数(数字),并返回他们的差值：(x, y) -> x – y
  * 4. 接收2个int型整数,返回他们的和： (int x, int y) -> x + y
  * 5. 接受一个 string 对象,并在控制台打印,不返回任何值(看起来像是返回void)：(String s) -> System.out.print(s)
+ * 省略写法：
+ * 1.括号中参数列表的参数类型，可以省略不写；
+ * 2.如果{}中只有一行代码，无论是否有返回值，都可以省略{}，return，分号。
  */
 
 public class Demo02Lambda {
@@ -16,7 +19,8 @@ public class Demo02Lambda {
             System.out.println(Thread.currentThread().getName() + "Lambda表达式代码生成线程!");
         }).start();
 
-        new Thread(()-> System.out.println(Thread.currentThread().getName()+"Lambda表达式生成代码【2】！")).start();
+        //省略格式的Lambda表达式
+        new Thread(() -> System.out.println(Thread.currentThread().getName() + "Lambda表达式生成代码【2】！")).start();
     }
 
 }
---
+++ Basicode/src/com/sunrise/day16/Demo01File.java
@@ -0,0 +1,42 @@
+package com.sunrise.day16;
+
+import java.io.File;
+
+/**
+ * java.io.File类：操作文件与文件夹
+ * 作用：文件增删，判断是否存在，文件遍历，文件大小；
+ * 绝对路径：E:\\Test\\Dictionaries\\text.sql
+ * 相对路径：text.sql，省略掉根目录
+ * 注意：1.路径不区分大小写；2.windows中路径\\两个反斜线代表\，是转义字符；
+ */
+public class Demo01File {
+    public static void main(String[] args) {
+        //路径分隔符 windows; linux：
+        String pathSeparator = File.pathSeparator;
+        System.out.println(pathSeparator);
+        //文件分隔符 windows\ linux /
+        String separator = File.separator;
+        System.out.println(separator);
+        System.out.println("================");
+        show01();
+        show02("//u01", "//test.sql");
+        show03();
+    }
+
+    private static void show01() {
+        File file = new File("c:\\text\\text\\a.txt");
+        System.out.println(file);
+    }
+
+    private static void show02(String parent, String child) {
+        File file = new File(parent, child);
+        System.out.println(file);
+    }
+
+    private static void show03() {
+        File parent  = new File("c:\\");
+        File file = new File(parent,"child.sql");
+        System.out.println(file);
+    }
+
+}
